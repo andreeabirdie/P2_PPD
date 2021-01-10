8aa3b34d8e01d00ed952e4277cd8c8553aea518d
Andrei Verdes <56959092+VNAndrei@users.noreply.github.com>
2021-01-10 15:16:22 +0200

    final

--- PPD_P2/kernel.cu
+++ PPD_P2/kernel.cu
@@ -1,8 +1,4 @@
-﻿#include <SDKDDKVer.h>
-
-#include <stdio.h>
-#include <tchar.h>
-#include "cuda_runtime.h"
+﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
 #include "cuda_runtime.h"
@@ -10,117 +6,88 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/video/tracking.hpp>
-#include <chrono> 
 
-typedef unsigned char uchar;
-typedef unsigned int uint;
-
-#define BLOCK_SIZE 32
-#define CV_64FC1 double
-#define CV_32F float
-#define CV_8U uchar
-
-int iDivUp(int a, int b)
+int divCeil(int a, int b)
 {
-	return ((a % b) != 0) ? (a / b + 1) : (a / b);
+	if ((a % b) != 0)
+		return a / b + 1;
+	return a / b;
 }
 
-__device__
-int absGrad(int grad) {
-	if (grad < 0) {
-		return -1 * grad;
-	}
-	else {
-		return grad;
-	}
-}
-
-__global__ void Kernel_Sobel(uchar* img, uchar* imgout, int ImgWidth, int imgHeigh)
+__global__ void kernel(unsigned char* imgIn, unsigned char* imgOut, int width, int height)
 {
-	int ImgNumColonne = blockIdx.x * blockDim.x + threadIdx.x;
-	int ImgNumLigne = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int line = blockIdx.y * blockDim.y + threadIdx.y;
 
-	int Index = (ImgNumLigne * ImgWidth) + (ImgNumColonne);
+	int Index = (line * width) + (col);
 
-	if ((ImgNumColonne < ImgWidth - 2) && (ImgNumLigne < imgHeigh - 2))
+	if ((col < width - 2) && (line < height - 2))
 	{
 		int i = Index;
-		int gradX = img[i] * -1 + img[i + 1] * 0 + img[i + 2] * 1;
-		i = ((ImgNumLigne + 1) * ImgWidth) + (ImgNumColonne);
-		gradX += img[i] * -2 + img[i + 1] * 0 + img[i + 2] * 2;
-		i = ((ImgNumLigne + 2) * ImgWidth) + (ImgNumColonne);
-		gradX += img[i] * -1 + img[i + 1] * 0 + img[i + 2] * 1;
+		int Gx = imgIn[i] * 1 + imgIn[i + 1] * 0 + imgIn[i + 2] * -1;
+		i = ((line + 1) * width) + (col);
+		Gx += imgIn[i] * 2 + imgIn[i + 1] * 0 + imgIn[i + 2] * -2;
+		i = ((line + 2) * width) + (col);
+		Gx += imgIn[i] * 1 + imgIn[i + 1] * 0 + imgIn[i + 2] * -1;
 
 		i = Index;
-		int gradY = img[i] * -1 + img[i + 1] * -2 + img[i + 2] * -1;
-		i = ((ImgNumLigne + 1) * ImgWidth) + (ImgNumColonne);
-		gradY += img[i] * 0 + img[i + 1] * 0 + img[i + 2] * 0;
-		i = ((ImgNumLigne + 2) * ImgWidth) + (ImgNumColonne);
-		gradY += img[i] * 1 + img[i + 1] * 2 + img[i + 2] * 1;
+		int Gy = imgIn[i] * 1 + imgIn[i + 1] * 2 + imgIn[i + 2] * 1;
+		i = ((line + 1) * width) + (col);
+		Gy += imgIn[i] * 0 + imgIn[i + 1] * 0 + imgIn[i + 2] * 0;
+		i = ((line + 2) * width) + (col);
+		Gy += imgIn[i] * -1 + imgIn[i + 1] * -2 + imgIn[i + 2] * -1;
 
-
-		int grad = absGrad(gradX) + absGrad(gradY);
-		int norm = grad * 0.0625;
-
-		imgout[Index] = norm;
+		imgOut[Index] = sqrtf(Gx * Gx + Gy * Gy) * 0.25;
 	}
 
 	return;
 }
 
-extern "C" bool apply_Sobel(cv::Mat * imgTresh, cv::Mat * Grayscale)
+extern "C" bool apply_Sobel(cv::Mat * inputImage, cv::Mat * outputImage)
 {
-
-	//	1. Initialize data
 	cudaError_t cudaStatus;
-	uchar* gDevImage;
-	uchar* gDevImageOut;
+	unsigned char* deviceIn;
+	unsigned char* deviceOut;
+	int BLOCK_SIZE = 32;
 
-	uint imageSize = imgTresh->rows * imgTresh->step1();
-	uint gradientSize = imgTresh->rows * imgTresh->cols * sizeof(uchar);
+	unsigned int imageSize = inputImage->rows * inputImage->cols * sizeof(unsigned char);
+	unsigned int gradientSize = inputImage->rows * inputImage->cols * sizeof(unsigned char);
 
 	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-	dim3 dimGrid(iDivUp(imgTresh->cols, BLOCK_SIZE), iDivUp(imgTresh->rows, BLOCK_SIZE));
+	dim3 dimGrid(divCeil(inputImage->cols, BLOCK_SIZE), divCeil(inputImage->rows, BLOCK_SIZE));
 
-	//	2. Allocation data
-	cudaStatus = cudaMalloc(&gDevImage, imageSize);
-	cudaStatus = cudaMalloc(&gDevImageOut, gradientSize);
+	cudaStatus = cudaMalloc(&deviceIn, imageSize);
+	cudaStatus = cudaMalloc(&deviceOut, gradientSize);
 
-	//	3. Copy data on GPU
-	cudaStatus = cudaMemcpy(gDevImage, imgTresh->data, imageSize, cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy(deviceIn, inputImage->data, imageSize, cudaMemcpyHostToDevice);
 
-	// Record start time
-	auto start = std::chrono::high_resolution_clock::now();
-
-	//	4. Launch kernel
-	Kernel_Sobel << <dimGrid, dimBlock >> > (gDevImage, gDevImageOut, imgTresh->step1(), imgTresh->rows);
+	kernel << <dimGrid, dimBlock >> > (deviceIn, deviceOut, inputImage->step1(), inputImage->rows);
 
 	cudaStatus = cudaGetLastError();
+
 	if (cudaStatus != cudaSuccess)
 	{
 		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		goto Error;
+		cudaFree(deviceIn);
+		cudaFree(deviceOut);
+
+		return cudaStatus;
 	}
-	//Wait for the kernel to end
+
 	cudaStatus = cudaDeviceSynchronize();
 	if (cudaStatus != cudaSuccess)
 	{
 		fprintf(stderr, "cudaDeviceSynchronize failed!");
-		goto Error;
+		cudaFree(deviceIn);
+		cudaFree(deviceOut);
+
+		return cudaStatus;
 	}
 
-	// Record end time
-	auto finish = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> elapsed = finish - start;
-	//std::cout << "Elapsed time GPGPU: " << elapsed.count() << " s\n";
+	cudaStatus = cudaMemcpy(outputImage->data, deviceOut, gradientSize, cudaMemcpyDeviceToHost);
 
-	//	5. Copy data on CPU
-	cudaStatus = cudaMemcpy(Grayscale->data, gDevImageOut, gradientSize, cudaMemcpyDeviceToHost);
-
-	//	6. Free GPU memory
-Error:
-	cudaFree(gDevImage);
-	cudaFree(gDevImageOut);
+	cudaFree(deviceIn);
+	cudaFree(deviceOut);
 
 	return cudaStatus;
 }
--- PPD_P2/main.cpp
+++ PPD_P2/main.cpp
@@ -1,26 +1,19 @@
-#include <SDKDDKVer.h>
-
-#include <stdio.h>
-#include <tchar.h>
-
 #include <string>
 #include <iostream>
 #include <opencv2/core/core.hpp> 
 #include <opencv2/highgui/highgui.hpp> 
 #include <opencv2/imgproc/types_c.h> 
 #include <opencv2/imgproc/imgproc.hpp> 
-#include <cmath>
-#include <chrono>  
 
 using namespace cv;
 
-extern "C" bool apply_Sobel(cv::Mat * imgTresh, cv::Mat * Grayscale);
+extern "C" bool apply_Sobel(cv::Mat * inputImage, cv::Mat * outputImage);
 
 int main()
 {
-	String pathToImage = "D:\\ppd\\P2_PPD\\PPD_P2\\yacc.jpg";
+	String pathToImage = "C:\\Users\\Andrei\\source\\repos\\P2_PPD\\PPD_P2\\yacc.jpg";
 
-	Mat inputImg = imread(pathToImage, 0); 
+	Mat inputImg = imread(pathToImage, 0);
 	Mat filteredImg = inputImg.clone();
 	apply_Sobel(&inputImg, &filteredImg);
 	imshow("Edgy yacc", filteredImg);
